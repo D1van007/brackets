63bcb2019198ee3f5816d9a3804d9c5968a1887f
D1van007 <d1van28.01.1987@gmail.com>
2022-08-02 18:52:24 +0300

    update: finished task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
+
+  const twin = new Object();
+   bracketsConfigX = bracketsConfig.flat(Infinity);
+  
+  
+  for (let i=0; i<bracketsConfigX.length; i=i+2){
+     twin[bracketsConfigX[i+1]] = bracketsConfigX[i]
+  }
+
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      if (stack.length !== 0 && twin[str[i]] == stack[stack.length-1]){
+        stack.pop()} 
+      else {stack.push(str[i])}
+      }
+   
+return stack.length == 0 
+
+
+
   // your solution
 }
